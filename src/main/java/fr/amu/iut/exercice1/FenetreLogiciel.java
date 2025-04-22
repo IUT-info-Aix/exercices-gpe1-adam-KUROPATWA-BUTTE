f66354b7869bdb8ff52b33ca6d9fe0ee1ecdb0ab
Adam <adam.kuropatwa-butte@etu.univ-amu.fr>
2025-04-22 14:30:22 +0200

    Ajout d'un premier bouton

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,13 +1,23 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+
 
 public class FenetreLogiciel extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // code de l'exercice 1
+        BorderPane root = new BorderPane();
+        root.setTop(new Button("File"));
+
+        Scene scene = new Scene(root, 100, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
